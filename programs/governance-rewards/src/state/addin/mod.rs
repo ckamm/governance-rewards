266900f5e6faeffd1c0e2d4d02bbfc61f0b9155e
Christian Kamm <mail@ckamm.de>
2022-05-26 11:44:27 +0200

    readd code for review

---
+++ programs/governance-rewards/src/state/addin/mod.rs
@@ -0,0 +1,110 @@
+mod temp_override;
+
+use std::ops::{Deref, DerefMut};
+
+use anchor_lang::prelude::*;
+
+pub use temp_override::VoterWeightAction;
+
+use crate::error::GovernanceRewardsError;
+
+use super::distribution::Distribution;
+
+#[derive(Clone)]
+pub struct VoterWeightRecord(temp_override::VoterWeightRecord);
+
+impl AccountDeserialize for VoterWeightRecord {
+    fn try_deserialize_unchecked(buf: &mut &[u8]) -> Result<Self> {
+        let record =
+            AnchorDeserialize::deserialize(buf).map_err(|_| ErrorCode::AccountDidNotDeserialize)?;
+        Ok(VoterWeightRecord(record))
+    }
+}
+
+impl AccountSerialize for VoterWeightRecord {}
+
+impl Deref for VoterWeightRecord {
+    type Target = temp_override::VoterWeightRecord;
+
+    fn deref(&self) -> &Self::Target {
+        &self.0
+    }
+}
+
+impl DerefMut for VoterWeightRecord {
+    fn deref_mut(&mut self) -> &mut Self::Target {
+        &mut self.0
+    }
+}
+
+pub struct Unvalidated<T> {
+    inner: T,
+}
+
+impl Unvalidated<VoterWeightRecord> {
+    pub fn validate(
+        self,
+        distribution: &Account<Distribution>,
+        registrant: &Pubkey,
+    ) -> Result<VoterWeightRecord> {
+        require!(
+            self.inner.is_not_expired(Clock::get()?),
+            GovernanceRewardsError::OutdatedVoteWeightRecord
+        );
+        require!(
+            self.inner.weight_action == Some(VoterWeightAction::RegisterForRewards),
+            GovernanceRewardsError::WrongAction
+        );
+        require!(
+            self.inner.weight_action_target == Some(distribution.key()),
+            GovernanceRewardsError::WrongActionTarget
+        );
+        require!(
+            self.inner.realm == distribution.realm,
+            GovernanceRewardsError::WrongRealm
+        );
+        require!(
+            self.inner.governing_token_owner == registrant.key(),
+            GovernanceRewardsError::WrongRegistrant
+        );
+
+        Ok(self.inner)
+    }
+}
+
+impl VoterWeightRecord {
+    pub fn is_not_expired(&self, clock: Clock) -> bool {
+        match self.voter_weight_expiry {
+            Some(slot) => slot >= clock.slot,
+            None => true,
+        }
+    }
+
+    pub fn try_from(account: &AccountInfo<'_>) -> Result<Unvalidated<Self>> {
+        let mut data: &[u8] = &account.try_borrow_data()?;
+        VoterWeightRecord::try_deserialize(&mut data)
+            .map(|record| Unvalidated { inner: record })
+            .map_err(|_| ErrorCode::AccountDidNotDeserialize.into())
+    }
+
+    pub fn create_test(
+        realm: Pubkey,
+        governing_token_mint: Pubkey,
+        governing_token_owner: Pubkey,
+        distribution: Pubkey,
+        weight: u64,
+        expiry: Option<u64>,
+    ) -> Self {
+        VoterWeightRecord(temp_override::VoterWeightRecord {
+            account_discriminator: temp_override::VoterWeightRecord::ACCOUNT_DISCRIMINATOR,
+            realm,
+            governing_token_mint,
+            governing_token_owner,
+            voter_weight: weight,
+            voter_weight_expiry: expiry,
+            weight_action: Some(temp_override::VoterWeightAction::RegisterForRewards),
+            weight_action_target: Some(distribution),
+            reserved: [0_u8; 8],
+        })
+    }
+}

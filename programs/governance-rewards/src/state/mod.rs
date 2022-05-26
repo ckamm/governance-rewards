01aac0dd4db652453ff968cd01801c2afe62624f
Christian Kamm <mail@ckamm.de>
2022-05-26 11:43:15 +0200

    delete all

--- programs/governance-rewards/src/state/mod.rs
+++
@@ -1,6 +0,0 @@
-pub mod addin;
-pub mod claim_data;
-pub mod distribution;
-pub mod distribution_option;
-pub mod preferences;
-pub mod total_rewards;
--- programs/governance-rewards/tests/program_test/mod.rs
+++
@@ -1,4 +0,0 @@
-pub mod governance_rewards_test;
-pub mod governance_test;
-pub mod program_test_bench;
-pub mod tools;

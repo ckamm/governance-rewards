6260ea48844042c4f71bcaf99cc3a2474e1d753b
Christian Kamm <mail@ckamm.de>
2022-06-13 14:20:53 +0200

    delete all

--- programs/governance-rewards/src/instructions/preferences/mod.rs
+++
@@ -1,5 +0,0 @@
-pub mod set_preferred_mint;
-pub mod set_resolution_preference;
-
-pub use set_preferred_mint::*;
-pub use set_resolution_preference::*;
--- programs/governance-rewards/src/instructions/reclaim/mod.rs
+++
@@ -1,5 +0,0 @@
-pub mod reclaim_funds;
-pub mod reclaim_user_data;
-
-pub use reclaim_funds::*;
-pub use reclaim_user_data::*;
